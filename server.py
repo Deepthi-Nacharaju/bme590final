2b135b648702e9cef3b533b5b8ba02fc6e308ad5
Deepthi Nacharaju <dnacharaju@gmail.com>
2018-11-29 20:13:06 -0500

    add validation function skeleton and mongoDB to class ImageDB

--- server.py
+++ server.py
@@ -1,34 +1,48 @@
 from flask import Flask, jsonify, request
 import requests
-
+import pymodm
+from pymodm import connect
+from pymodm import MongoModel, fields
 app = Flask(__name__)
 
-@app.route("/process/type/<id>", methods=["GET"])
-def get_process_type(id):
-    if id is 1:
-        histogram_equalization()
-    elif id is 2:
-        contrast_switch()
-    elif id is 3:
-        log_compression()
-    elif id is 4:
-        reverse_video()
+
+class ImageDB(MongoModel):
+    images = fields.ListField()
+
+
+@app.route("/process", methods=["POST"])
+def get_process_type():
+    r = request.get_json()
+    process_id = r['process_id']
+    image_file = r['image_file']
+    validate_image()
+    if process_id is 1:
+        histogram_equalization(image_file)
+    elif process_id is 2:
+        contrast_switch(image_file)
+    elif process_id is 3:
+        log_compression(image_file)
+    elif process_id is 4:
+        reverse_video(image_file)
     else:
         return jsonify('Not a valid ID')
     return jsonify('YAY!')
 
 
-def histogram_equalization():
+def validate_image(image_file):
+    return
+
+def histogram_equalization(image_file):
     return
 
 
-def contrast_switch():
+def contrast_switch(image_file):
     return
 
 
-def log_compression():
+def log_compression(image_file):
     return
 
 
-def reverse_video():
+def reverse_video(image_file):
     return
