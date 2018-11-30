1b380a314ffd1112577df93716909ac2593eb8af
Deepthi Nacharaju <dnacharaju@gmail.com>
2018-11-29 21:49:40 -0500

    merge server script with Ashish's stuff

--- server.py
+++ server.py
@@ -3,11 +3,85 @@ import requests
 import pymodm
 from pymodm import connect
 from pymodm import MongoModel, fields
+import datetime
+import logging
+logging.basicConfig(filename='log.txt', level=logging.DEBUG, filemode='w')
 app = Flask(__name__)
 
 
 class ImageDB(MongoModel):
+    name = fields.CharField(primary_key=True)
+    original = fields.ImageField()
+    actions = fields.ListField()
+    histogram_count = fields.IntegerField()
+    contrast_count = fields.IntegerField()
+    log_count = fields.IntegerField()
+    reverse_count = fields.IntegerField()
     images = fields.ListField()
+    processor = fields.ListField()
+    images_time_stamp = fields.ListField()
+
+
+@app.route("/", methods=["GET"])
+def greeting():
+    """ Welcomes user to image processor
+
+    This function returns the following string: "Welcome to the image
+    processor"
+
+    Returns:
+        welcome (string): "Welcome to the image processor"
+    """
+
+    welcome = "Welcome to the image processor"
+    return jsonify(welcome)
+
+
+@app.route("/data/<image_id>", methods=["GET"])
+def getData(image_id):
+    """
+    This function returns all the stored information for a patient
+    as a JSON dictionary
+
+    Args:
+        image_id (string): string specifying image name.
+
+    Returns:
+        dict_array (dict): stored information for specified image
+    """
+
+    u = ImageDB.objects.raw({"_id": image_id}).first()
+    dict_array = {
+                 }
+    return jsonify(dict_array)
+
+
+@app.route("/new_image", methods=["POST"])
+def addimage():
+    """
+
+    Args:
+
+    Returns:
+
+    """
+
+    now = datetime.now()
+    data_in = request.get_json()
+    required_image_keys = [
+
+    ]
+    for key in required_image_keys:
+        if key not in data_in.keys():
+            raise ValueError("Key '{}' is missing"
+                             " to initialize image"
+                             " in imageprocessor".format(key))
+    u = ImageDB(
+             )
+    logging.debug("Image {} was"
+                  " successfully initialized".format(u.name))
+    u.save()
+    return u.name
 
 
 @app.route("/process", methods=["POST"])
@@ -59,3 +133,9 @@ def log_compression(image_file):
 
 def reverse_video(image_file):
     return
+
+
+if __name__ == "__main__":
+    connect("mongodb://bme590:Dukebm3^@ds253889.mlab.com:53889/imageprocessor")
+    # app.run(host="127.0.0.1")
+    app.run(host="0.0.0.0")
