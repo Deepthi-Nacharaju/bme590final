0f67157e23543955f601649b5a5945af69ef8af1
Ashish Vankara <ashishvankara@gmail.com>
2018-12-10 16:47:07 -0500

    implemented unit testing for encode function

--- test_server.py
+++ test_server.py
@@ -17,8 +17,10 @@ def test_validate_image():
 #    assert image_bytes == image_bytes2
 
 
-# def test_encode_file_as_b64():
-#    encoded = encode_file_as_b64('Dogs.jpg')
-#    with open('Dogs.jpg', "rb") as image_file:
-#        encoded2 = base64.b64encode(image_file.read())
-#    assert encoded == encoded2
+def test_encode_file_as_b64():
+    from skimage import io as io2
+    dog = io2.imread('Dogs.jpg')
+    b64 = encode_file_as_b64(dog)
+    correctdogb64 = open('dogb64.txt', "r")
+    correctb64 = correctdogb64.read()
+    assert b64 == correctb64
