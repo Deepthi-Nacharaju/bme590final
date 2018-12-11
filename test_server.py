744dbb0c8028b9c416c4af0efaa713b1a2e4e55c
Ashish Vankara <ashishvankara@gmail.com>
2018-12-10 22:16:41 -0500

    implemented testing for decode function

--- test_server.py
+++ test_server.py
@@ -4,17 +4,20 @@ from server import decode_b64_image
 import base64
 import io
 import matplotlib.image as mpimg
+import numpy as np
 
 
 def test_validate_image():
     assert True
 
 
-# def test_decode_b64_image():
-#    encoded = encode_file_as_b64('Dogs.jpg')
-#    image_bytes = base64.b64decode(encoded)
-#    image_bytes2 = decode_b64_image(encoded)
-#    assert image_bytes == image_bytes2
+def test_decode_b64_image():
+    from skimage import io as io2
+    dog = io2.imread('Dogs.jpg')
+    correctdogb64 = open('dogb64.txt', "r")
+    correctb64 = correctdogb64.read()
+    dog_decoded = decode_b64_image(correctb64)
+    assert np.array_equal(dog, dog_decoded)
 
 
 def test_encode_file_as_b64():
